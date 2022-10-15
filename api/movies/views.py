fefe62eab9d33fcce71975ab7985b81ec4108d59
divyansh pandey <pdivyansh946@gmail.com>
2022-10-15 13:53:50 +0530

    first commit

---
+++ api/movies/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from . serializers import MovieSerializers
+from .models import Moviedata
+# Create your views here.
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset=Moviedata.objects.all()
+    serializer_class= MovieSerializers
+
+class ActionViewSet(viewsets.ModelViewSet):
+    queryset = Moviedata.objects.filter(typ='action')
+    serializer_class = MovieSerializers
+
+class ComedyViewSet(viewsets.ModelViewSet):
+    queryset=Moviedata.objects.filter(typ='comedy')
+    serializer_class= MovieSerializers
